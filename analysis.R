9e73c9604ad62995c373740751d9a46fc4824387
AlexLee1999 <chunyen.alex.lee@gmail.com>
2021-10-14 12:35:36 +0800

    add sink functions

--- analysis.R
+++ analysis.R
@@ -3,8 +3,21 @@ d <- read.csv("./data_cities/taipei.csv")
 for (url in data_cities_list){
     d<-rbind(d, read.csv(url))
 }
-# model_first <- lm(debtpp ~ unem + revenuepp + socialwalfarepp + elecyr + party + unanimous + relation + expect + unem:elecyr + lowincomehh, data = d)
-# summary(model_first)
 
+### FIRST MODEL
+sink(file="./Results/OLS_R_model1.txt")
+model_first <- lm(debtpp ~ unem + revenuepp + socialwalfarepp + elecyr + party + unanimous + relation + expect + unem:elecyr + lowincomehh, data = d)
+summary(model_first)
+sink(file=NULL)
+
+
+### SECOND MODEL
+sink(file="./Results/OLS_R_model2.txt")
 model_second <- lm(debtpp ~ unem + elecyr + cpi + population + socialwalfarepp + unanimous + revenuepp + lowincomehh + expect + party + relation + dpp + kmt + y67 + y68 + y69 + y70 + y71 + y72 + y73 + y74 + y75 + y76 + y77 + y78 + y79 + y80 + y81 + y82 + y83 + y84 + y85 + y86 + y87 + y88 + y89 + y90 + y91 + y92 + y93 + y94 + y95 + y96 + y97 + y98 + y99 + y100 + y101 + y102 + y103 + y104 + y105 + y106 + y107 + y108 + chc + tw + hcc + tpe + ntpc + tao + tcc + tnn + kao + kee + yil, data = d)
 summary(model_second)
+sink(file=NULL)
+
+sink(file="./Results/OLS_R_model3.txt")
+model_third <- lm(debtpp ~ unem + revenuepp + socialwalfarepp + elecyr + party + unanimous + relation + expect + unem:elecyr + lowincomehh + y67 + y68 + y69 + y70 + y71 + y72 + y73 + y74 + y75 + y76 + y77 + y78 + y79 + y80 + y81 + y82 + y83 + y84 + y85 + y86 + y87 + y88 + y89 + y90 + y91 + y92 + y93 + y94 + y95 + y96 + y97 + y98 + y99 + y100 + y101 + y102 + y103 + y104 + y105 + y106 + y107 + y108 + chc + tw + hcc + tpe + ntpc + tao + tcc + tnn + kao + kee + yil, data = d)
+summary(model_third)
+sink(file=NULL)
